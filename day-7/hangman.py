a9924782702fcd7d673b7ecd10fac67132f0e77e
kmarreroe <karel.marrero@thoughtworks.com>
2022-10-24 23:56:30 -0500

    End hangman. Init day 8, func with param

--- day-7/hangman.py
+++ day-7/hangman.py
@@ -1,64 +1,10 @@
 import random
+from hangman_art import logo, hanstages 
+from hangman_words import word_list
 
-world_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(world_list)
+print(logo)
+chosen_word = random.choice(word_list)
 print(f"Test: Chosen word is: {chosen_word}")
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
 
 lives = 6
 word_length = len(chosen_word)
@@ -66,12 +12,14 @@ display = []
 for i in range(word_length):    # Range from 0 -> word_length excluded
     display.append("_") # Same as display += "_"
 
-# print(display)
-# print(stages[lives])
 while "_" in display and lives > 0:
     print(display)
-    print(stages[lives])
+    print(hanstages[lives])
     guess = input("Guess a letter: ").lower()    
+
+    if guess in display:
+      print(f"You've already guessed {guess}")
+      continue
 
     # for letter in chosen_word:
     is_letter_present = False
@@ -82,11 +30,12 @@ while "_" in display and lives > 0:
             is_letter_present = True
     
     if not is_letter_present:
-        lives -= 1
+      print(f"You guessed{guess}, that's not in the world. You lose a life.")
+      lives -= 1
         
 
 if lives == 0:
-    print(stages[lives])
+    print(hanstages[lives])
     print("You lose!")
 else:
     print(display)
---
+++ day-8/main.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hello John")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+
+greet()
+
+
+def great_with_name(name):
+    print(f"Hello {name}")
+    print("How do you do?")
+    print("Isn't the weather nice today?")
+
+great_with_name("Jane")
