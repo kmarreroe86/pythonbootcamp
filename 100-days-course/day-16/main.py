b50734485506b9177856c3eb96fa5ff70f7c0a3a
kmarreroe <karel.marrero@thoughtworks.com>
2023-02-05 17:58:17 -0500

    OOP

---
+++ 100-days-course/day-16/main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("darkGreen")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+# import turtle
+# star = turtle.Turtle()
+
+# star.right(75)
+# star.forward(100)
+
+# for i in range(4):
+#     star.right(144)
+#     star.forward(100)
+
+# turtle.done()
