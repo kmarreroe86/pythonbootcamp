e5df4ecff8265351eddeb3acf86442d71e427ec2
kmarreroe <karel.marrero@thoughtworks.com>
2023-02-16 10:42:17 -0500

    Snake game end.

--- 100-days-course/day-20/snake_game.py
+++ 100-days-course/day-20/snake_game.py
@@ -1,23 +1,48 @@
-from turtle import Turtle, Screen
-
-
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-snake = []
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-for position in STARTING_POSITIONS:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    snake.append(new_segment)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
+    # Detect collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect wall collision
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with the tail(any segment which is not the head -> trigger Game Over)
+    # Check against all segments starting at position 1. Python slicing
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
