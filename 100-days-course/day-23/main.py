47e261fa751964222050d69bfbbb18c3e0c50116
Karel <kmarrero@visualaimsoft.com>
2023-02-18 15:46:53 -0500

    Crossing game end

--- 100-days-course/day-23/main.py
+++ 100-days-course/day-23/main.py
@@ -9,12 +9,30 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
-
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    #       Detect successful crossing
+    if player.is_at_finish_line():
+        player.go_at_start_position()
+        car_manager.level_up_speed()
+        scoreboard.increase_level()
+
+        #     Detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
--- 100-days-course/day-23/player.py
+++ 100-days-course/day-23/player.py
@@ -11,8 +11,14 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_at_start_position()
         self.setheading(90)
 
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    def go_at_start_position(self):
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        return True if self.ycor() > FINISH_LINE_Y else False
