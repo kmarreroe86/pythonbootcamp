7a4d3ff01c141446267a6b4a103a1af733fe3a0e
kmarreroe <karel.marrero@thoughtworks.com>
2023-06-05 15:10:58 -0500

    password generator end

--- 100-days-course/day-29/main.py
+++ 100-days-course/day-29/main.py
@@ -1,10 +1,62 @@
 from tkinter import *
+from tkinter import messagebox
+import pyperclip
+import random
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-# ---------------------------- UI SETUP ------------------------------- #
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = random.choices(letters, k=nr_letters)
+    password_symbols = random.choices(symbols, k=nr_symbols)
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]  # another way using list comprehension
+
+    result_list = password_letters + password_symbols + password_numbers
+    random.shuffle(result_list)
+
+    password_generated = "#".join(result_list)
+    print(f"Your password is: {password_generated}")
+    password_entry.delete(0, END)
+    password_entry.insert(0, password_generated)
+    pyperclip.copy(password_generated)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if not website or not password:
+        messagebox.showinfo(title="Missing values", message="Some fields are empty")
+        return
+
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                          f"\nPassword: {password} \nIs ok to save")
+    if not is_ok:
+        return
+
+    with open("data.txt", "a") as data_file:  # open file in append mode, if not exists it will be created
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        email_entry.delete(0, END)
+        email_entry.insert(0, "email.gmail.com")
+        password_entry.delete(0, END)
+
+
+# ---------------------------- UI SETUP ------------------------------- #R
 
 window = Tk()
 window.title("Password Manager")
@@ -30,13 +82,13 @@ website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "email.gmail.com")
-password_entry = Entry(width=17)
+password_entry = Entry(width=18)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
